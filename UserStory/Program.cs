62d08105ea412820629aa36c4d3cf13b14235f58
TAugust <taugust@noreply.codeberg.org>
2026-06-10 17:09:05 +0200

    implemented uml

---
+++ UserStory/Program.cs
@@ -0,0 +1,85 @@
+﻿class Gruppe {
+    String name;
+    List<String> teams;
+    public void addTeam(String teamName) {}
+}
+
+class Spiel {
+    String spielId;
+    DateTime datum;
+    DateTime uhrzeit;
+    String ergebnis;
+    String heimMannschaft;
+    String auswaertsMannschaft;
+    
+    public void setErgebnis(String score)
+    {
+        ergebnis = score;
+    }
+    
+    public String getErgebnis()
+    {
+        return ergebnis;
+    }
+}
+
+class Benutzer {
+    String name;
+    double guthaben;
+    
+    public void updateGuthaben(double amount)
+    {
+        guthaben = amount;
+    }
+}
+
+class Wette {
+    String wettTyp;
+    double quote;
+    double einsatz;
+    bool istAusgewertet;
+    
+    public double auswerten(String ergebnis)
+    {
+        return 0.0 * ergebnis.Length;
+    }
+}
+
+class TurnierManager {
+    List<Gruppe> gruppen;
+    List<Spiel> spiele;
+    List<Benutzer> benutzer;
+    List<Wette> wetten;
+
+    public void saveToJson(String filePath) {
+        
+    }
+
+    public void loadFromJson(String filePath) {
+
+    }
+
+    public void createNewTournament() {
+
+    }
+
+    public void printGames() {
+
+    }
+
+    public void setQuote(String spielId, String typ, double quote) {
+
+    }
+
+    public double getQuote(String spielId, String typ) {
+        return 0.0;
+    }
+
+    public void placeBid(String playerName, String spielId, String typ, double amount) {
+        Wette wette = new();
+    }
+    
+    public void processResult(String spielId, String score) {
+
+    }
+}
